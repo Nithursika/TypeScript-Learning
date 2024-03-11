c8a1d3b8cee2bce1f99a04a677743ff88b89193c
Nithursika <nithursika.nk@gmail.com>
2024-03-11 19:03:04 +0530

    functions l-4

--- build/js/main.js
+++ build/js/main.js
@@ -1,55 +1,16 @@
 "use strict";
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // let username = 'Nithu'
 // console.log(username)
-// let a: number = 12
-// let b: number = 6                 //   boolean,string,any
-// console.log(a / b)
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// const sum = (a: number, b: number) => {
-//     return a + b
-// }
-// console.log(sum(10, 11))
-// let postId: number | string;  //union type
-// let re: RegExp = /\w+/g
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////// Arrays ////////
-let stringArr = ["aa", "bb", "cc", "ss"];
-let unionTypeArr = [1, "aa"]; //let unionTypeArr: (string | number)[]=
-let mixData = [1, "aa", true];
-unionTypeArr[0] = "aaaaa";
-unionTypeArr.push(234);
-unionTypeArr.unshift("www");
-console.log(unionTypeArr);
-/////// Tuples ////////
-let myTuple = ['Dave', 24, true];
-mixData = myTuple;
-// myTuple = mixData   //cannot assign 
-////// object ///////
-let myobj;
-myobj = [];
-console.log(typeof myobj);
-const exampleObj = {
-    prop1: "Nithu",
-    prop2: 24,
+/////// Literal Types ///////
+let myName;
+let username;
+username = 'Dave';
+/////// function ///////
+const add = (a, b) => {
+    return a + b;
 };
-let evh = {
-    name: "nithu",
-    active: true,
-    album: ['aa', 12]
+const logMsg = (message) => {
+    console.log(message);
 };
-const greetGuitarist = (guitarist) => {
-    return `Hello ${guitarist.name}!`;
-};
-console.log(greetGuitarist(evh));
-/////// Enums ///////
-// "Unlike  most Typescript features, enums are not a type-level addition to javascript but something added to the languge and runtime"
-var Grade;
-(function (Grade) {
-    Grade[Grade["U"] = 0] = "U";
-    Grade[Grade["D"] = 1] = "D";
-    Grade[Grade["C"] = 2] = "C";
-    Grade[Grade["B"] = 3] = "B";
-    Grade[Grade["A"] = 4] = "A";
-})(Grade || (Grade = {}));
-console.log(Grade.U); //0
-console.log(Grade.A); //4
+logMsg("hhhhh");
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // let username = 'Nithu'
 // console.log(username)
 
 // let a: number = 12
 // let b: number = 6                 //   boolean,string,any
 // console.log(a / b)
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // const sum = (a: number, b: number) => {
 //     return a + b
 // }
@@ -14,63 +17,91 @@
 // let postId: number | string;  //union type
 
 // let re: RegExp = /\w+/g
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////// Arrays ////////
-let stringArr = ["aa", "bb", "cc", "ss"]
-let unionTypeArr = [1, "aa"]             //let unionTypeArr: (string | number)[]=
-let mixData = [1, "aa", true]
+// let stringArr = ["aa", "bb", "cc", "ss"]
+// let unionTypeArr = [1, "aa"]             //let unionTypeArr: (string | number)[]=
+// let mixData = [1, "aa", true]
 
-unionTypeArr[0] = "aaaaa"
-unionTypeArr.push(234)
-unionTypeArr.unshift("www")
+// unionTypeArr[0] = "aaaaa"
+// unionTypeArr.push(234)
+// unionTypeArr.unshift("www")
 
-console.log(unionTypeArr)
-/////// Tuples ////////
-let myTuple: [string, number, boolean] = ['Dave', 24, true]
+// console.log(unionTypeArr)
+// /////// Tuples ////////
+// let myTuple: [string, number, boolean] = ['Dave', 24, true]
 
-mixData = myTuple
-// myTuple = mixData   //cannot assign 
+// mixData = myTuple
+// // myTuple = mixData   //cannot assign
 
-////// object ///////
-let myobj: object
-myobj = []
-console.log(typeof myobj)
+// ////// object ///////
+// let myobj: object
+// myobj = []
+// console.log(typeof myobj)
 
-const exampleObj = {
-    prop1: "Nithu",
-    prop2: 24,
-}
+// const exampleObj = {
+//     prop1: "Nithu",
+//     prop2: 24,
+// }
 
-// exampleObj.prop1=12  //cannot assign
+// // exampleObj.prop1=12  //cannot assign
 
-type Guitarist = {                           // can use     interface Guitarist{ }
+// type Guitarist = {                           // can use     interface Guitarist{ }
+//     name: string,
+//     active?: boolean,
+//     album: (string | number)[],
+// }
+
+// let evh: Guitarist = {
+//     name: "nithu",
+//     active: true,
+//     album: ['aa', 12]
+
+// }
+
+// const greetGuitarist = (guitarist: Guitarist) => {
+//     return `Hello ${guitarist.name}!`
+// }
+// console.log(greetGuitarist(evh))
+
+// /////// Enums ///////
+// // "Unlike  most Typescript features, enums are not a type-level addition to javascript but something added to the languge and runtime"
+
+// enum Grade {
+//     U,
+//     D,
+//     C,
+//     B,
+//     A,
+// }
+// console.log(Grade.U)   //0
+// console.log(Grade.A)  //4
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////// Type Aliases ///////
+type stringOrNumberArray = (string | number)[]
+type Guitarist = {
     name: string,
     active?: boolean,
-    album: (string | number)[],
+    album: stringOrNumberArray,
 }
 
-let evh: Guitarist = {
-    name: "nithu",
-    active: true,
-    album: ['aa', 12]
+/////// Literal Types ///////
+let myName: "nithu"
+let username: 'Dave' | 'joe' | 'nithu'
+username = 'Dave'
 
+/////// function ///////
+const add = (a: number, b: number) => {
+    return a + b
+}
+const logMsg = (message: any): void => {
+    console.log(message)
 }
 
-const greetGuitarist = (guitarist: Guitarist) => {
-    return `Hello ${guitarist.name}!`
-}
-console.log(greetGuitarist(evh))
-
-/////// Enums ///////
-// "Unlike  most Typescript features, enums are not a type-level addition to javascript but something added to the languge and runtime"
-
-enum Grade {
-    U,
-    D,
-    C,
-    B,
-    A,
-}
-console.log(Grade.U)   //0
-console.log(Grade.A)  //4
+logMsg("hhhhh")
